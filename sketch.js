4e06a8cb131b8dbc6256753810ee37175f0ce085
Cosmicguard1an <cosmicguardian1@gmail.com>
2020-11-17 21:56:57 -0800

    hi

--- sketch.js
+++ sketch.js
@@ -7,9 +7,10 @@ const Constraint = Matter.Constraint;
 
 
 
-var ground1
+var ground1,ground2
 var polygon
 var box1,box2,box3,box4,box5,box6,box7,box8,box9
+var box10,box11,box12,box13,box14,box15,box16,box17,box18
 
 var polygonObject
 var gameState = "onSling";
@@ -19,7 +20,7 @@ function setup() {
   engine = Engine.create();
   world = engine.world
   ground1 = new Ground(470, 400, 200, 15);
-
+  ground2 = new Ground(675,350,200,15);
   
   
   //first layer
@@ -35,6 +36,24 @@ function setup() {
  
   //third layer
   box9= new Box(470,280,35,35);
+
+
+
+  //Second ground thingy
+  
+  //first layer
+  box10 = new Box(610,315,35,35);
+  box11 = new Box(645,315,35,35);
+  box12 = new Box(680,315,35,35);
+  box13 = new Box(715,315,35,35);
+  box14 = new Box(750,315,35,35);
+  //Second Layer
+  box15 = new Box(645,280,35,35);
+  box16 = new Box(680,280,35,35);
+  box17 = new Box(715,280,35,35);
+
+  //Third Layer
+  box18 = new Box(680,245,35,35);
 
   var options = {
     restitution: 0.3,
@@ -57,6 +76,7 @@ function draw() {
   background(255,255,255);  
 
   ground1.display();
+  ground2.display();
   box1.display();
   box2.display();
   box3.display();
@@ -66,6 +86,16 @@ function draw() {
   box7.display();
   box8.display();
   box9.display();
+  box10.display();
+  box11.display();
+  box12.display();
+  box13.display();
+  box14.display();
+  box15.display();
+  box16.display();
+  box17.display();
+  box18.display();
+  
   ellipseMode(RADIUS)
   ellipse(polygon.position.x,polygon.position.y,20,20);
   polygonObject.display();
